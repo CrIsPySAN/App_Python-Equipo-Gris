509676f97f2f4e17ab412996805a7dd2e88e03f9
Diego Pedraza <diegopedrazaut@gmail.com>
2025-01-21 19:41:48 -0600

    Modificaciones de Diego Pedraza 2.0

---
+++ backend/models/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_wtf.csrf import CSRFProtect
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+csrf = CSRFProtect()
+
+def create_app(config_class="instance.config.Config"):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    csrf.init_app(app)
+    CORS(app)  # Protege contra ataques Cross-Origin Resource Sharing
+    
+    from ...app.views import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
+@app.after_request
+def apply_security_headers(response):
+    response.headers["X-Frame-Options"] = "DENY"
+    return response
